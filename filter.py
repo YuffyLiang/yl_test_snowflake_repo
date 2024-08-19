fa77c65aa72261dfcd86be22d841dc98b3cde821
YuffyLiang <yuffy.liang@mantelgroup.co.nz>
2024-08-20 11:02:13 +1200

    Update filter.py

--- filter.py
+++ filter.py
@@ -3,4 +3,3 @@ from snowflake.snowpark.functions import col
 def filter_by_menu(session, table_name, menu_type):
   df = session.table(table_name)
   return df.filter(col("menu_type") == menu_type)
-';
